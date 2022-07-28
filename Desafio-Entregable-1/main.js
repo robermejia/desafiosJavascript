1989e7a14a214957031194dffcb394e2d4b68b7f
Roberto Mejía <robertomejiacollazos@gmail.com>
2022-07-28 18:20:00 -0500

    Desafios

---
+++ Desafio-Entregable-1/main.js
@@ -0,0 +1,40 @@
+// DESAFÍO ENTREGABLE Nº 1
+// Como fue anticipado, aquí finaliza el primer módulo y se entregarán 
+// las consignas del primer desafío entregable del curso. El mismo, incluirá 
+// temas vistos en las clases 1, 2, 3 y 4.
+/* ////////////////////////////// SIMULADOR INTERACTIVO ////////////////////////////// */
+
+// Formato: Página HTML y código fuente en JavaScript. Debe identificar el apellido 
+// del alumno/a en el nombre de archivo comprimido por “claseApellido”.
+
+// Sugerencia: Algunos criterios a tener en cuenta para seleccionar un proceso a 
+// simular por primera vez son: 
+// “ELEGIR UN PROCESO BIEN CONOCIDO”: Si conozco una situación que implique adquirir 
+// cierta información y estoy bien familiarizado en “cómo se hace”, es más fácil 
+// traducir la solución a un lenguaje de programación.
+// “ELEGIR UN PROCESO QUE ME RESULTE INTERESANTE” : Si me siento motivado sobre el 
+// tema, es más llevadero enfrentar los retos de desarrollo e interpretación. 
+// Antes de programar existe la etapa de relevamiento y análisis que me permite 
+// identificar cómo solucionar el proceso.
+/* ////////////////////////////// SIMULADOR INTERACTIVO ////////////////////////////// */
+
+// >> Consigna: Con los conocimientos vistos hasta el momento, empezarás a armar la estructura 
+// inicial de tu proyecto integrador. A partir de los ejemplos mostrados la primera clase, 
+// deberás:
+// Pensar el alcance de tu proyecto: ¿usarás un cotizador de seguros? ¿un simulador de créditos? 
+// ¿un simulador personalizado?
+// Armar la estructura HTML del proyecto.
+// Incorporar lo ejercitado en las clases anteriores, algoritmo condicional y algoritmo con ciclo.
+// Utilizar funciones para realizar esas operaciones.
+// >>Aspectos a incluir en el entregable:
+// Archivo HTML y Archivo JS, referenciado en el HTML por etiqueta <script src="js/miarchivo.js">
+// </script>, que incluya la definición de un algoritmo en JavaScript que emplee funciones 
+// para resolver el procesamiento principal del simulador
+// >>Ejemplo:
+// Calcular costo total de productos y/o servicios seleccionados por el usuario.
+// Calcular pagos en cuotas sobre un monto determinado.
+// Calcular valor final de un producto seleccionado en función de impuestos y descuentos.
+// Calcular tiempo de espera promedio en relación a la cantidad de turnos registrados.
+// Calcular edad promedio de personas registradas.
+// Calcular nota final de alumnos ingresados.
+
