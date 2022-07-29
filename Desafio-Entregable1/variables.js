7e10b1d2bd41309805de843234d0810c758276ff
Roberto Mejía <robertomejiacollazos@gmail.com>
2022-07-28 20:47:57 -0500

    Agregando variables

---
+++ Desafio-Entregable1/variables.js
@@ -0,0 +1,76 @@
+// MENSAJES
+let presentacion = "La gastronomía peruana también se disfruta por las mañanas.";
+
+let mensajeOpciones =   "Escribir del 1 al 3" + "\n" +
+"Escribir 1: Ver la lista de desayunos." + "\n" + 
+"Escribir 2: Ver la lista de almuerzos" + "\n" + 
+"Escribir 3: Salir." 
+
+// CALCULOS
+let cuentaDesayuno = 0;
+let cuentaAlmuerzo = 0;
+
+// DESAYUNO
+let panConChicharon = 2;
+let panConPejerrey = 3;
+let butifarra = 2;
+let salchichaHuachana = 2;
+let Sangrecita = 2.5;
+let lomoAlJugo = 3;
+let caldoDeCabeza = 2.5;
+let tamal = 2;
+let humita = 2;
+
+// ALMUERZO
+let ceviche = 35;
+let polloALaBrasa = 25;
+let causaRellena = 24;
+let papaALaHuancaina = 23;
+let ajiDeGallina = 25;
+let anticuchos = 15;
+let lomoSaltado = 35;
+let arrozConPollo = 25;
+let pachamanca = 40;
+
+// DESCRIPCIÓN DESAYUNO
+let panConChicharonDescripción = "Es de los desayunos más deseados. Los comensales suelen pedirlo en formato de sándwich, el cual incluye camote frito, morcilla y zarza criolla. El Pan con chicharrón va perfecto con un vaso de café con leche o con jugo natural.";
+
+let panConPejerreyDescripción = "Es un platillo muy solicitado en zonas costeras, donde se pesca el pejerrey fresco.";
+
+let butifarraDescripción = "El ingrediente principal para la Butifarra peruana es el jamón del país.";
+
+let salchichaHuachanaDescripción = "Este embutido le debe su peculiar nombre a la ciudad de Huacho, provincia norteña donde se hizo popular. Está hecho de carne, grasa de cerdo y semillas de achiote, que le otorgan su característico color anaranjado. Se puede comer frito, combinado con pan, o revuelto con huevo. Se puede acompañar con café o jugo natural.";
+
+let SangrecitaDescripción = "Alimento muy nutritivo, que se utiliza en el país para combatir la anemia.";
+
+let lomoAlJugoDescripción = "Si eres un amante del Lomo saltado, vas a disfrutar como nadie de este desayuno. El Lomo al jugo es un émulo del Lomo saltado. Ya lo verás.";
+
+let caldoDeCabezaDescripción = "Esta sopa es reponedora, sobre todo después de una jornada de desvelo. Su elaboración es laboriosa, pero el resultado es espectacular.";
+
+let tamalDescripción = "Este preparado a base de maíz es típico de un desayuno familiar de domingo.";
+
+let humitaDescripción = "Es la ‘hermana’ del tamal, aunque su sabor puede ser salado o dulce. Elaborada a base de maíz, su contenido puede variar. En las Humitas saladas, viene queso y carne de pollo o de  chancho. En las dulces, en cambio, solo se mezcla el maíz con el azúcar.";
+
+
+// DESCRIPCIÓN ALMUERZO
+
+let cevicheDescripcion = "El representante gastronómico peruano más reconocido a nivel internacional. Motivo de orgullo y símbolo de identidad nacional. Es tanto su éxito que es servido con diversas variaciones en otros países de Latinoamérica.";
+
+let polloALaBrasaDescripcion = "Este plato típico de bandera es el más consumido en el Perú, su exquisito sabor hace que sea el favorito cuando las familias peruanas se reúnen. El pollo macerado es cocinado a las brasas en un horno especial,";
+
+let causaRellenaDescripcion = "Normalmente se puede encontrar este plato en 2 presentaciones: “Causa rellena de pollo” y “Causa rellena de atún”, siendo las dos sumamente deliciosas.";
+
+let papaALaHuancainaDescripcion = "Se puede consumir como entrada o como plato de fondo. Este exquisito platillo tiene sus orígenes en la ciudad de Huancayo, departamento de Junín.";
+
+let ajiDeGallinaDescripcion = "Es un plato fusión de ingredientes quechuas y españoles. Este manjar típico de la gastronomía peruana consta de una crema espesa hecha con gallina desmenuzada, pan, ají verde, leche y caldo.";
+
+let anticuchosDescripcion = "Hechos en base a corazón de res, aunque no suene muy apetitoso, ¡es realmente delicioso! Ensartados en palos de caña y aderezados con ají panca, los anticuchos suelen servirse acompañados de choclo, papas doradas o sancochadas y ají. ";
+
+let lomoSaltadoDescripcion = "Un tradicional platillo criollo del Perú. Hecho en base a trozos de carne, cebolla, tomate y papas fritas. Particular por su distinta técnica de preparación, colocando todos los ingredientes en la sartén bien caliente y sazonando con pimienta, ajinomoto y comino.";
+
+let arrozConPolloDescripcion = "Arroz graneado con perejil y espinaca (lo que le da el color verde) y cocinado con alverjitas, zanahorias y presas de pollo. Suele acompañarse con Papa a la Huancaína y ser decorada con cebolla.";
+
+let pachamancaDescripcion = "Uno de los platos más tradicionales de la gastronomía de la sierra peruana. Su forma de preparar es bastante particular, todos sus ingredientes entran en cocción en piedras precalentadas colocadas en la tierra.";
+
+
+
