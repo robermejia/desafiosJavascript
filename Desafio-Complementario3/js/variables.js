93e04edd201609b9041038f1366c35c1ac66f900
Roberto Mejía <robertomejiacollazos@gmail.com>
2022-08-13 11:22:43 -0500

    Implementando DOM y EVENTOS

---
+++ Desafio-Complementario3/js/variables.js
@@ -0,0 +1,10 @@
+let carritoDeCompras = []
+
+const $contenedorDeCards = document.getElementById('contenedor-productos');
+const $contenedorDeCarrito = document.getElementById('contenedorCarrito');
+
+const $contadorCarrito1 = document.getElementById('contadorCarrito1');
+const $contadorCarrito2 = document.getElementById('contadorCarrito2');
+const $precioTotal = document.getElementById('precioTotal');
+
+const $tipoDeConsumo = document.getElementsByClassName('form-select');
